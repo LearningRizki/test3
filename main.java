56ea9f5413da90164ef641b2d9266b8a5dcb2360
iman fauzi <imanfauzi29@gmail.com>
2022-05-22 13:56:05 +0700

    merging

--- main.java
+++ main.java
@@ -2,15 +2,24 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String nama, email;
-        System.out.println("Masukan Data User");
-        System.out.print("Nama : ");
-        nama = in.nextLine();
-        System.out.print("Email : ");
-        email = in.next();
-        
-        print(nama, email);
+        try (Scanner in = new Scanner(System.in)) {
+            String nama, email;
+            System.out.println("Masukan Data User");
+            System.out.print("Nama : ");
+            nama = in.nextLine();
+            System.out.print("Email : ");
+            email = in.next();
+            
+            print(nama, email);
+
+            // fungsi pertambahan
+            System.out.println("\nPertambahan");
+            System.out.print("Bilangan pertama: ");
+            int a = in.nextInt();
+            System.out.print("Bilangan kedua: ");
+            int b = in.nextInt();
+            System.out.println("Hasil: " + substract(a, b));
+        }
     }
 
     public static void print(String nama, String email) {
@@ -20,9 +29,9 @@ public class main {
         System.out.println("Email : " + email);
     }
 
-    public static void substract(int a, int b) {
+    public static int substract(int a, int b) {
         System.out.println("\n");
         System.out.println("Hasil Pengurangan");
-        System.out.println("Hasil : " + (a - b));
+        return a - b;
     }
 }
