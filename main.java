263a96befb364fe1022f6121c8883df5cc2d6c10
Rizki Ramdhani <rizkiramdhani564@gmail.com>
2022-05-22 13:17:07 +0700

    User

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class main{
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String nama,email;
+    System.out.println("Masukan Data User");
+    System.out.print("Nama : ");
+     nama = in.nextLine();
+    System.out.print("Email : ");
+     email = in.next();
+     
+    System.out.println("\n");
+     System.out.println("Data User");
+     System.out.println("Nama  : "+nama);
+     System.out.println("Email : "+email);
+
+}
+}
